6d37ed586be4116b58b3af3b622c871bd20bd467
Arnaud Senegas <arnaud.senegas@hotmail.com>
2026-07-27 11:58:31 +0200

    Refactor: split oc_lettings into lettings and profiles apps

--- lettings/views.py
+++ lettings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Letting
+
+
+def index(request):
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings/index.html', context)
+
+
+def letting(request, letting_id):
+    letting = Letting.objects.get(id=letting_id)
+    context = {
+        'title': letting.title,
+        'address': letting.address,
+    }
+    return render(request, 'lettings/letting.html', context)
