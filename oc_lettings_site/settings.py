a194a9906156367b22c277a5498f54c92f1f5e69
Arnaud Senegas <arnaud.senegas@hotmail.com>
2026-08-04 14:14:34 +0200

    Fix flake8 error

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-#ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 
